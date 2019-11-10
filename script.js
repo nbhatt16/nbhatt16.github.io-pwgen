14e60867a6ab4f6c746739f9173a86a286b54182
Neal Bhatt <nealyb@gmail.com>
2019-11-10 16:37:08 -0500

    index/script

---
+++ script.js
@@ -0,0 +1,40 @@
+function generate(){
+
+    var complexity = document.getElementById("slider").value;
+
+    var values = "ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
+
+    var password = "";
+
+    for(var i = 0; i <= complexity; i++){
+        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+    }
+
+    document.getElementById("display").value = password;
+
+    document.getElementById("lastNums").innerHTML += password + "<br />";
+
+}
+
+document.getElementById("length").innerHTML = "Length: 128";
+
+document.getElementById("slider").oninput = function(){
+
+    if(document.getElementById("slider").value > 0){
+        document.getElementById("length").innerHTML = "Length: " + document.getElementById("slider").value;
+    }
+    else{
+        document.getElementById("length").innerHTML = "Length: 1";
+    }
+
+}
+
+function copyPassword(){
+
+    document.getElementById("display").select();
+
+    document.execCommand("Copy");
+
+    alert("Password copied to clipboard!");
+
+}
